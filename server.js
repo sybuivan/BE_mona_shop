4a589b7ed62a1845f6fe909274dfe42b07f86171
buivansy <your github email address>
2023-01-05 14:03:25 +0700

    fix env

--- server.js
+++ server.js
@@ -59,4 +59,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => console.log('Server is running on port 5000'));
